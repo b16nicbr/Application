0b95ea2bd57e5bb9b5e5a64a33cf94d0e37ee855
Nicklas Branding <branding96@gmail.com>
2023-03-20 20:53:01 +0100

    added logging for resapis

--- ApplicationAPI/src/main/java/com/applicationapi/controllers/UserController.java
+++ ApplicationAPI/src/main/java/com/applicationapi/controllers/UserController.java
@@ -4,12 +4,14 @@ import com.applicationapi.controllers.models.UserRequest;
 import com.applicationapi.controllers.models.UserResponse;
 import com.applicationpersistence.entity.User;
 import com.applicationservices.services.impl.UserServiceImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/it-systems")
+@Slf4j
 public class UserController {
 
     @Autowired UserServiceImpl userService;
@@ -20,8 +22,10 @@ public class UserController {
         User user = userService.findById(id);
 
         if(user == null){
+            log.warn("User :{} does not exist", user.getName());
             return ResponseEntity.notFound().build();
         }
+        log.info("Found user :{}", user.getName());
         return ResponseEntity.ok(new UserResponse(user.getId(), user.getName(), user.getAge(), user.getAccesslevel()));
     }
 
